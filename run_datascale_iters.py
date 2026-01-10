3650c7f570088bf621b656a92829eb8044fd2bb4
Jedelm23 <52426090+Jedelm23@users.noreply.github.com>
2026-01-10 17:00:39 -0500

    run_datascale_iters.py: scale up data via max iterations, from one large dataset (scaling up epochs)

---
+++ run_datascale_iters.py
@@ -0,0 +1,30 @@
+import subprocess
+from pathlib import Path
+
+OUT_DIR = Path("runs/datascale_iters")
+
+MAX_ITERS_LIST = [100, 300, 1000, 3000, 10000] #up to 10M tokens total
+SEEDS = [0] # ignore for now; going to do multiple seeds later
+
+def run_experiment(max_iters, seed):
+    tag = f"maxsteps_{max_iters}_seed_{seed}"
+    out_dir = OUT_DIR / tag
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    cmd = [
+        "python", "train.py",
+        "config/train_synthetic_data.py",      # base config
+        #f"--out_dir={out_dir}",          # nanoGPT supports this override
+        #f"--seed={seed}", # add in seed ability later
+        f"--max_iters={max_iters}", # override max iters
+        f"--wandb_run_name=hhmm-12-2-1M-{max_iters}", # unique wandb name
+        f"--warmup_iters={max_iters // 20}", # override warmup iters
+        f"--lr_decay_iters={max_iters}", # override lr decay iters
+    ]
+    print("Running:", " ".join(map(str, cmd)))
+    subprocess.run(cmd, check=True)
+
+if __name__ == "__main__":
+    for max_iters in MAX_ITERS_LIST:
+        for seed in SEEDS:
+            run_experiment(max_iters, seed)
